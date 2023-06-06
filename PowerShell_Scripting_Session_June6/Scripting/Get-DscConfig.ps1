9bb3e265d2db75dac72fc3da08b5ced08ea74598
imcuteani <anindita.basak@live.com>
2023-06-06 06:56:50 +0000

    added PS scripts

---
+++ PowerShell_Scripting_Session_June6/Scripting/Get-DscConfig.ps1
@@ -0,0 +1,34 @@
+﻿# Install PowerShell DSC module 
+
+Install-Module 'PSDscResources' -Verbose
+
+# Define the DSC configuration and generate the configuration doc 
+
+Configuration EnvironmentVariable_Path
+{
+    param ()
+
+    Import-DscResource -ModuleName 'PSDscResources'
+
+    Node localhost
+    {
+        Environment CreatePathEnvironmentVariable
+        {
+            Name = 'TestPathEnvironmentVariable'
+            Value = 'TestValue'
+            Ensure = 'Present'
+            Path = $true
+            Target = @('Process', 'Machine')
+        }
+    }
+}
+
+EnvironmentVariable_Path -OutputPath:"./EnvironmentVariable_Path"
+
+# Apply Configuration to the machine 
+
+Start-DscConfiguration -Path 'C:\Users\adminuser\EnvironmentVariable_Path' -Wait -Verbose
+
+# Get the current status of the configuration 
+
+Get-DscConfiguration
